a2403b7f9aacdaa72757aabc3b3e59f7a454801b
xwatson <xuwusheng@hotmail.com>
2018-04-30 18:28:07 +0800

    取消使用cheerio

--- search/searchMobile.js
+++ search/searchMobile.js
@@ -35,35 +35,30 @@ class search {
         return await this.getDetail(detailContent)
     }
     async getDetail(content) {
-        let mgtvUrl = null,
-            mgtvIndex = -1
-        const playerSource_DC = await content.findElement(By.id('con_vod_1'))
-        const playTitles = await playerSource_DC.findElements(By.className('play-title'))
-        let index = 0
-        for (let item in playTitles) {
-            let playTvText = await playTitles[item].getText()
-            if (playTvText.indexOf('芒芒TV') > -1) {
-                mgtvIndex = index
-                break
-            }
-            index++
-        }
-        if (mgtvIndex > -1) {
-            let mgtvBox_DC = await playerSource_DC.findElements(By.className('play-box'))
-            let mgtvs_DC = await mgtvBox_DC[mgtvIndex].findElement(By.className('plau-ul-list')).findElements(By.tagName('li'))
-            for(let item in mgtvs_DC) {
-                let mgtv_text = await mgtvs_DC[item].getText()
-                if (mgtv_text.indexOf('在线观看') > -1) {
-                    await mgtvs_DC[item].findElement(By.tagName('a')).click()
-                    break
-                }
-            }
-            await this.driver.sleep(2000)
-            await this.driver.switchTo().frame(1)
-            await this.driver.wait(until.elementLocated(By.id('J_miPlayerWrapper')), 1000*60)
-            mgtvUrl = await this.driver.findElement(By.id('J_miPlayerWrapper')).findElement(By.tagName('video')).getAttribute('src')
-        }
-        return mgtvUrl
+        let detail = {}
+        const movieBody = await content.findElement(By.id('resize_vod'))
+        const infoBody = await movieBody.findElement(By.className('vod-n-l'))
+        detail.title = (await infoBody.findElement(By.tagName('h1')).getText()).replace(/\n|/g, '').trim()
+        const infos = await infoBody.findElements(By.tagName('p'))
+        detail.status = (await infos[0].getText()).replace(/\n|状态：/g, '')
+        detail.starring = (await infos[1].getText()).replace(/\n|主演：/g, '')
+        detail.type = (await infos[2].getText()).replace(/\n|类型：/g, '')
+        detail.director = (await infos[3].getText()).replace(/\n|导演：/g, '')
+        detail.area = (await infos[4].getText()).replace(/\n|地区：/g, '')
+        detail.updateTime = (await infos[5].getText()).replace(/\n|更新：/g, '')
+        detail.updateTime = moment(`20${detail.updateTime}`).format('YYYY-MM-DD HH:mm:ss')
+        const playTabs = await content.findElement(By.className('vod-play-tab')).findElements(By.tagName('li'))
+        await playTabs[1].findElement(By.tagName('span')).click()
+        await this.driver.wait(until.elementIsVisible(content.findElement(By.id('con_vod_2'))), 3000)
+        const plot = await content.findElement(By.id('con_vod_2')).getText()
+        detail.plot = plot.replace(/\n|/g, '')
+        await movieBody.findElement(By.className('vod_play')).findElement(By.tagName('a')).click()
+        await this.driver.sleep(1000)
+        await this.driver.switchTo().frame(1)
+        await this.driver.wait(until.elementLocated(By.id('J_miPlayerWrapper')), 1000*60)
+        detail.playerUrl = await this.driver.findElement(By.id('J_miPlayerWrapper')).findElement(By.tagName('video')).getAttribute('src')
+        return detail
+        
     }
 }
 
